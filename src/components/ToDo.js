b5a9efe17b81fdd62d03aab23df373280fbd6f8b
peter_alfie <alfie1104@gmail.com>
2020-06-05 13:29:00 +0900

    Done

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { connect } from "react-redux";
+import { actionCreators } from "../store";
+import { Link } from "react-router-dom";
 
-function ToDo({ text }) {
+function ToDo({ text, onBtnClick, id }) {
   return (
     <li>
-      {text} <button>DEL</button>
+      <Link to={`/${id}`}>
+        {text} <button onClick={onBtnClick}>DEL</button>
+      </Link>
     </li>
   );
 }
 
-export default ToDo;
+//redux의 dispatch함수와 부모 컴포넌트로 부터 전달받은 props를 이용하여,
+//함수를 만들고 props에 할당하였음
+function mapDispatchToProps(dispatch, ownProps) {
+  return {
+    onBtnClick: () => dispatch(actionCreators.deleteToDo(ownProps.id)),
+  };
+}
+
+export default connect(null, mapDispatchToProps)(ToDo);
