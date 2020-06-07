dedc5a15700cca6d9486cb9eaca3ed80174f9e63
peter_alfie <alfie1104@gmail.com>
2020-06-08 08:56:55 +0900

    Redux toolkit is applied

--- src/store.js
+++ src/store.js
@@ -1,34 +1,37 @@
 import { createStore } from "redux";
+import { createAction, createReducer, configureStore } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
+/* 
+  //===createStore를 이용하면 아래와 같이 switch case를 안 써도 됨 ===/
 
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id: parseInt(id),
-  };
-};
-
-const reducer = (state = [], action) => {
+  const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
+    case addToDo.type:
+      return [{ text: action.payload, id: Date.now() }, ...state];
+    case deleteToDo.type:
+      return state.filter((toDo) => toDo.id !== action.payload);
     default:
       return state;
   }
-};
+}; */
 
-const store = createStore(reducer);
+/*
+  뭔가를 return해야하면 new state를 리턴하고,
+  return 하지 않고 그냥 state를 mutate할 수도 있음
+  state를 mutate하면 내부적으로 immer가 new state를 리턴함
+*/
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) =>
+    state.filter((toDo) => toDo.id !== action.payload),
+});
+
+const store = configureStore({ reducer });
 
 export const actionCreators = {
   addToDo,
